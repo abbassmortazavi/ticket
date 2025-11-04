b3f279d35106b3db0fb1855e7606b104780f2ac6
Abbass Mortazavi <abbass.mortazavi@patoughi.info>
2025-11-04 15:18:25 +0330

    feat: create pkg for database

--- main.go
+++ main.go
@@ -1,50 +1,23 @@
 package main
 
 import (
-	"fmt"
-	"os"
 	"ticket/cmd/api"
 	"ticket/internal/auth"
-	"ticket/internal/db"
 	"ticket/internal/store"
 	"ticket/pkg/config"
+	"ticket/pkg/database"
+	"ticket/pkg/logger"
 
-	"github.com/rs/zerolog"
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/viper"
 )
 
 func main() {
 	config.Set()
-	// Initialize global logger
-	initGlobalLogger(viper.GetString("APP_DEBUG") == "true")
+	database.Connect()
+	logger.Init()
 
-	if viper.GetString("APP_DEBUG") == "true" {
-		log.Info().Msg("debug mode enabled")
-	}
-
-	var myHost string
-	if host := os.Getenv("DB_HOST"); host != "" {
-		myHost = host
-	} else {
-		myHost = viper.GetString("DB_HOST")
-	}
-
-	dsn := fmt.Sprintf("host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
-		myHost,
-		viper.GetString("DB_PORT"),
-		viper.GetString("DB_USERNAME"),
-		viper.GetString("DB_PASSWORD"),
-		viper.GetString("DB_NAME"))
-
-	log.Info().Msgf("dsn: %s", dsn)
-
-	database, err := db.New(dsn, viper.GetString("DB_MAX_IDLE_TIMEOUT"), viper.GetInt("DB_MAX_CONN"), viper.GetInt("DB_MAX_IDLE"))
-	if err != nil {
-		log.Fatal().Err(err).Msg("connect db failed")
-	}
-
-	storage := store.NewStorage(database)
+	storage := store.NewStorage(database.DB)
 	jwt := viper.GetString("JwtSecret")
 	authenticator := auth.NewJwtAuthenticator(jwt)
 
@@ -56,21 +29,5 @@ func main() {
 	mux := app.Start()
 	if err := app.Run(mux); err != nil {
 		log.Fatal().Err(err).Msg("server failed to start")
-	}
-}
-
-func initGlobalLogger(debug bool) {
-	// Console output for development
-	output := zerolog.ConsoleWriter{
-		Out:        os.Stdout,
-		TimeFormat: "2006-01-02 15:04:05",
-	}
-
-	log.Logger = zerolog.New(output).With().Timestamp().Logger()
-
-	if debug {
-		zerolog.SetGlobalLevel(zerolog.DebugLevel)
-	} else {
-		zerolog.SetGlobalLevel(zerolog.InfoLevel)
 	}
 }
